1103cde68b41c094723de95cf5f3c714285029cd
Pavel_Bladimir <pamiralhp@gmail.com>
2023-06-13 15:09:07 +0200

    Ejercicio-2

--- Ej-2/index.php
+++ Ej-2/index.php
@@ -9,4 +9,17 @@ echo "<br>" . $x;
 echo "<br>" . $y;
 echo "<br>" . $TF;
 echo "<br>" . NAME;
+
+$upperString = strtoupper($txt);
+echo $upperString . "<br>";
+
+$stringLength = strlen($txt);
+echo $stringLength . "<br>";
+
+$reversedString = strrev($txt);
+echo $reversedString . "<br>";
+
+$newString = "Este es el curso de PHP";
+$concatenatedString = $txt . " " . $newString;
+echo $concatenatedString . "<br>";
 ?>
