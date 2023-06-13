a2b72e1072165a0b1cc81a9d48da954cfc76a865
Pavel_Bladimir <pamiralhp@gmail.com>
2023-06-13 12:49:40 +0200

    Ejercicio-1

---
+++ Ej-2/index.php
@@ -0,0 +1,12 @@
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+$TF = true;
+const NAME = "Pavel";
+echo $txt;
+echo "<br>" . $x;
+echo "<br>" . $y;
+echo "<br>" . $TF;
+echo "<br>" . NAME;
+?>
