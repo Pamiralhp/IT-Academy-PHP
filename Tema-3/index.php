c50ae5c6210e75aecc6f7854b9a4955750bfb9b5
Pavel_Bladimir <pamiralhp@gmail.com>
2023-06-14 12:49:44 +0200

    Tema-3-Ejercicio-2

--- Tema-3/index.php
+++ Tema-3/index.php
@@ -6,5 +6,21 @@ foreach ($numeros as $key) {
     echo $key . "<br>";
 }
 
+echo "<h2>". "Ejercicio-2"."</h2>";
+
+$X = array(10, 20, 30, 40, 50, 60);
+
+print_r("<pre>");
+print_r($X);
+print_r("</pre>");
+echo "Tamaño del array original: " . count($X) . "<br>";
+
+unset($X[2]);
+
+$X = array_values($X);
+print_r("<pre>");
+print_r($X);
+print_r("</pre>");
+echo "Tamaño del array eliminando elemento: " . count($X) . "<br>";
 
 ?>
