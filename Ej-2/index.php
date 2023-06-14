7ad0e7889a453b3138027202f0a27d88d87dec15
Pavel_Bladimir <pamiralhp@gmail.com>
2023-06-14 09:28:14 +0200

    Ej-3-Calculadora

--- Ej-2/index.php
+++ Ej-2/index.php
@@ -1,17 +1,20 @@
 <?php
-//Ejercicio-1
-// $txt = "Hello world!";
-// $x = 5;
-// $y = 10.5;
-// $TF = true;
-// const NAME = "Pavel";
-// echo $txt;
-// echo "<br>" . $x;
-// echo "<br>" . $y;
-// echo "<br>" . $TF;
-// echo "<br>" . NAME;
 
-//Ejercicio-2
+define('STDIN',fopen("php://stdin","r"));
+echo "<h2>"."Ejercicio-1"."</h2>";
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+$TF = true;
+const NAME = "Pavel";
+echo $txt;
+echo "<br>" . $x;
+echo "<br>" . $y;
+echo "<br>" . $TF;
+echo "<br>" . NAME . "<hr>";
+
+echo "<h2>"."Ejercicio-2"."</h2>";
+
 $txt = "Hello world!";
 
 $upperCase = strtoupper($txt);
@@ -25,5 +28,61 @@ echo $reversed . "<br>";
 
 $text2 = "Este es el curso de PHP";
 $concatxt = $txt . " " . $text2;
-echo $concatxt . "<br>";
+echo $concatxt  . "<hr>";
+
+echo "<h2>"."Ejercicio-3"."</h2>";
+
+$x = 5;
+$y = 3;
+$a = 7.5;
+$b = 10.5;
+
+echo "Valor de X = " . $x . "<br>" .
+"Valor de Y = " . $y . "<br>" . 
+"Valor de A = " . $a . "<br>" . 
+"Valor de B = " . $b . "<hr>";
+
+echo "X + Y = " . $x + $y . "<br>";
+echo "X - Y = " . $x - $y . "<br>";
+echo "X x Y = " . $x * $y . "<br>";
+echo "X % Y = ". $x % $y  . "<br>" . "<hr>";
+
+echo "A + B = ". $a + $b . "<br>";
+echo "A - B = ". $a - $b . "<br>";
+echo "A x B = ". $a * $b . "<br>";
+echo "A % B = ". $a % $b . "<br>" . "<hr>";
+
+
+
+echo "Doable de X = " . (2 * $x). "<br>" .
+ "Doable de Y = " . (2 * $y). "<br>" . 
+ "Doable de A = " . (2 * $a). "<br>" . 
+ "Doable de B = " . (2 * $b). "<br>";
+echo "Suma de todas las variables = " . ($x + $y) + ($a + $b). "<br>";
+echo "Modulo de todas las variables = " . $x % $y % $a % $b. "<hr>";
+
+echo "<h2>"."Calculadora"."</h2>";
+
+
+function calculator($num1, $num2, $operator) {
+    switch ($operator) {
+        case '+':
+            return $num1 + $num2;
+        case '-':
+            return $num1 - $num2;
+        case '*':
+            return $num1 * $num2;
+        case '/':
+            if ($num2 == 0) {
+                return "Error: Cannot divide by zero";
+            } else {
+                return $num1 / $num2;
+            }
+        default:
+            return "Error: Invalid operator";
+    }
+}
+
+$result = calculator(23, 12,"+");
+echo "Resultado: " . $result ;
 ?>
