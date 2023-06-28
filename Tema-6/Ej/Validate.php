5502f4c572aae80187cfc9105c3493587117ed1c
Pamiralhp <132684622+Pamiralhp@users.noreply.github.com>
2023-06-28 10:22:51 +0200

    Tema-6-Ej-2

--- Tema-6/Ej/Validate.php
+++ Tema-6/Ej/Validate.php
@@ -1,9 +1,12 @@
+<?php
+session_start();
+?>
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?><br>
-Your gender is: <?php echo $_POST["gender"]; ?><br>
-Your Dni/Nie is: <?php echo $_POST["dninie"]; ?>
+Welcome <?php echo $_SESSION["name"] = $_POST["name"]; ?><br>
+Your email address is: <?php echo $_SESSION["email"] = $_POST["email"]; ?><br>
+Your gender is: <?php echo $_SESSION["gender"] = $_POST["gender"]; ?><br>
+Your Dni/Nie is: <?php echo $_SESSION["dninie"] = $_POST["dninie"]; ?>
 </body>
 </html>
