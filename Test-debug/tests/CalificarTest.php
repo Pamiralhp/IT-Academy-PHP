2c9d2ccac9b8dc7ec62758f4d3ccb6bdf30a00ec
Pamiralhp <132684622+Pamiralhp@users.noreply.github.com>
2023-07-10 09:56:53 +0200

    #

--- Test-debug/tests/CalificarTest.php
+++ Test-debug/tests/CalificarTest.php
@@ -4,9 +4,12 @@ include 'src\Calificar.php';
 use PHPUnit\Framework\TestCase;
 final class CalificarTest extends TestCase{
     public function testCalificar(){
-        $grade1 = new Calificar(33);
-        $grade2= new Calificar(45);
-        $grade3 = new Calificar(60);
+        $grade1 = 33;
+        $grade2 = 45;
+        $grade3 = 60;
+        $grade1 = new Calificar();
+        $grade2= new Calificar();
+        $grade3 = new Calificar();
         
 
         $grade1 = $grade1->Calificar(70);
