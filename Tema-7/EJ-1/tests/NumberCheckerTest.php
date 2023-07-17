1b75839b28e5a90ca9608462f3dc751bdc5003b1
Pamiralhp <132684622+Pamiralhp@users.noreply.github.com>
2023-07-17 09:51:10 +0200

    Tema-7_EJ-2

--- Tema-7/EJ-1/tests/NumberCheckerTest.php
+++ Tema-7/EJ-1/tests/NumberCheckerTest.php
@@ -7,20 +7,15 @@ final class NumberCheckerTest extends TestCase{
     public function testIsEven(){
 
         $number = new NumberChecker(2);
-        $number = new NumberChecker(9);
-    
-        $number->isEven();
-        $this->assertTrue($number,'El numero es par');
-        $this->assertFalse($number);
+        $number1 = new NumberChecker(9);
+
+        $this->assertTrue($number->isEven());
+        $this->assertFalse($number1->isEven());
     }
     public function testIsPositive(){
 
         $number = new NumberChecker(1);
-        $number = new NumberChecker(-7);
-        
-        $number->isPositive();
-        $this->assertTrue($number, 'El numero es positivo');
-        $this->assertFalse($number);
+        $this->assertTrue($number->isPositive());
     }        
 }
 
