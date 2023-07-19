4d63b0371cb9c8ae4095f2f984aac9d580ee26e6
Pamiralhp <132684622+Pamiralhp@users.noreply.github.com>
2023-07-19 12:40:41 +0200

    Tema-7_EJ-Test-debug

--- Test-debug/tests/CalificarTest.php
+++ Test-debug/tests/CalificarTest.php
@@ -5,15 +5,12 @@ use PHPUnit\Framework\TestCase;
 final class CalificarTest extends TestCase{
     public function testCalificar(){
         $grade = new Calificar();
-        $grade1 = new Calificar();
-        $grade2= new Calificar();
-        $grade3 = new Calificar();
         
         
         $this->assertEquals($grade->Calificar(20),"Reprobado");
-        $this->assertSame($grade1->Calificar(33),"Tercera Division");
-        $this->assertSame($grade2->Calificar(45),"Segunda Division");
-        $this->assertSame($grade3->Calificar(60),"Primera Division");
+        $this->assertSame($grade->Calificar(33),"Tercera Division");
+        $this->assertSame($grade->Calificar(45),"Segunda Division");
+        $this->assertSame($grade->Calificar(60),"Primera Division");
     }
 }
 
